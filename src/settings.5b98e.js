88fac097352b77f368436cafa49262577df4873c
zboboan <3878606@qq.com>
2018-07-29 13:54:57 +0800

    a

---
+++ src/settings.5b98e.js
@@ -0,0 +1,214 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "0": [
+                "resources/bj_1",
+                1,
+                1
+            ],
+            "2": [
+                "resources/ui_home",
+                1,
+                1
+            ],
+            "3": [
+                "resources/boom_3",
+                1,
+                1
+            ],
+            "5": [
+                "resources/ui_star",
+                1,
+                1
+            ],
+            "6": [
+                "resources/boom_2",
+                1,
+                1
+            ],
+            "7": [
+                "resources/boom_1",
+                1,
+                1
+            ],
+            "8": [
+                "resources/ske_anim/boom.json",
+                5
+            ],
+            "9": [
+                "resources/prefabs/balloon.prefab",
+                3
+            ],
+            "10": [
+                "resources/ske_anim/blingbling.json",
+                5
+            ],
+            "15x/QL+VhKk4t8vg44hblJ": [
+                "resources/audio/ad_1.mp3",
+                0
+            ],
+            "e9zUMjIiRG2YIzTx1YKFeP": [
+                "resources/audio/ad_5.mp3",
+                0
+            ],
+            "a8Jf8zQI1Pcq1nkDOzGWKU": [
+                "resources/audio/bg.mp3",
+                0
+            ],
+            "b8JV5sgO5CmLbgOkb4pNiv": [
+                "resources/bj_1.png",
+                2
+            ],
+            "c4UVa+3UFHVqLrGUWt6/VE": [
+                "resources/boom_1.png",
+                2
+            ],
+            "04FGouA5JMXa5tNxyLk4+w": [
+                "resources/boom_2.png",
+                2
+            ],
+            "326iOxdXNFG6SobbbGa5I3": [
+                "resources/boom_3.png",
+                2
+            ],
+            "2cNWSKqidE9pblLJ4FzvtB": [
+                "resources/ske_anim/blingbling",
+                1,
+                1
+            ],
+            "1dxDs4/BpGrZV2rkzQTu4o": [
+                "resources/ske_anim/blingbling.atlas",
+                4
+            ],
+            "a9izd15JpN0aRRYD//9vYI": [
+                "resources/ske_anim/blingbling.png",
+                2
+            ],
+            "d74858OoZF8qOqf6cBhasY": [
+                "resources/ske_anim/boom",
+                1,
+                1
+            ],
+            "b8pvXhns5LOZKYaT0tv1MR": [
+                "resources/ske_anim/boom.atlas",
+                4
+            ],
+            "085iTGvWtPhISQ6GnEtbNg": [
+                "resources/ske_anim/boom.png",
+                2
+            ],
+            "d9FCst29NGh5Z4Rb8LL0oX": [
+                "resources/ske_anim/boom2",
+                1,
+                1
+            ],
+            "12GTqnnZJPFIjm+xLqYiK9": [
+                "resources/ske_anim/boom2.png",
+                2
+            ],
+            "64pJp6vWNND7+Q466WVdRs": [
+                "resources/ske_anim/boom3",
+                1,
+                1
+            ],
+            "afaxDY+ylFTYKp3bauNOiI": [
+                "resources/ske_anim/boom3.png",
+                2
+            ],
+            "e2otV/zOtNeIuSUYcLsJ4d": [
+                "resources/ui_home.png",
+                2
+            ],
+            "352ErCTHNPYLhQGk9K49y0": [
+                "resources/ui_star.png",
+                2
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.AudioClip",
+        "cc.SpriteFrame",
+        "cc.Texture2D",
+        "cc.Prefab",
+        "cc.RawAsset",
+        "sp.SkeletonData"
+    ],
+    launchScene: "db://assets/scenes/game_scene.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/game_scene.fire",
+            uuid: 4
+        },
+        {
+            url: "db://assets/scenes/index_scene.fire",
+            uuid: 1
+        }
+    ],
+    packedAssets: {
+        "056585c98": [
+            0,
+            1,
+            2
+        ],
+        "05ca48780": [
+            0,
+            3,
+            4,
+            5,
+            6,
+            7
+        ],
+        "06dc900": [
+            8,
+            9,
+            10
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "01KTU2STNJ4JEN7hZ8jQ0n",
+        "1d1wl2tQ9DJLX594i1SDtX",
+        "edLjxJaWFKYKTYq0V7JWNG",
+        "28JMou9/JIV4dUOiKT/yta",
+        "290RisGftP1orsfxhjhLbn",
+        "b5JySOk6NM2oEOOU5mNn6V",
+        "b6mgaa99hMpLQXN9YGH7xd",
+        "eaV5RTQOpFgrDAtPp1++M6",
+        "95AjFnuBVJK4RTjg+mlnRV",
+        "951aKvhORACL6VDWYhU66i",
+        "96nuRIdMBA/5B5pxnPxSi5"
+    ],
+    "md5AssetsMap": {
+        "05/056585c98.json": "aefbf",
+        "05/05ca48780.json": "5242a",
+        "06/06dc900.json": "964bb",
+        "2c/2c35648a-aa27-44f6-96e5-2c9e05cefb41.json": "b9991",
+        "64/64a49a7a-bd63-4d0f-bf90-e3ae9655d46c.json": "c779c",
+        "d7/d7e3ce7c-3a86-45f2-a3aa-7fa70185ab18.json": "faaec",
+        "d9/d9142b2d-dbd3-4687-9678-45bf0b2f4a17.json": "60565",
+        "assets/resources/audio/ad_1.mp3": "47a2b",
+        "assets/resources/audio/ad_5.mp3": "f8f49",
+        "assets/resources/audio/bg.mp3": "bf630",
+        "assets/resources/bj_1.png": "9b4cf",
+        "assets/resources/boom_1.png": "d0b14",
+        "assets/resources/boom_2.png": "f7bf5",
+        "assets/resources/boom_3.png": "4b14a",
+        "assets/resources/ske_anim/blingbling.atlas": "581e1",
+        "assets/resources/ske_anim/blingbling.png": "e8265",
+        "assets/resources/ske_anim/boom.atlas": "c1d1e",
+        "assets/resources/ske_anim/boom.png": "36ab1",
+        "assets/resources/ske_anim/boom2.png": "b9302",
+        "assets/resources/ske_anim/boom3.png": "66e7c",
+        "assets/resources/ui_home.png": "19702",
+        "assets/resources/ui_star.png": "14968"
+    }
+};
