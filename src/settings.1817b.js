a0b76202c05fed60f204ee5256fad452a09fb047
zboboan <3878606@qq.com>
2018-08-04 19:22:47 +0800

    a

---
+++ src/settings.1817b.js
@@ -0,0 +1,656 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "0": [
+                "resources/textures/e3",
+                1,
+                1
+            ],
+            "1": [
+                "resources/textures/e7",
+                1,
+                1
+            ],
+            "2": [
+                "resources/textures/e6",
+                1,
+                1
+            ],
+            "3": [
+                "resources/textures/splode_5",
+                1,
+                1
+            ],
+            "4": [
+                "resources/textures/splode_3",
+                1,
+                1
+            ],
+            "5": [
+                "resources/textures/e5",
+                1,
+                1
+            ],
+            "6": [
+                "resources/textures/splode_7",
+                1,
+                1
+            ],
+            "7": [
+                "resources/textures/splode_1",
+                1,
+                1
+            ],
+            "8": [
+                "resources/textures/splode_4",
+                1,
+                1
+            ],
+            "9": [
+                "resources/textures/splode_6",
+                1,
+                1
+            ],
+            "10": [
+                "resources/textures/e4",
+                1,
+                1
+            ],
+            "11": [
+                "resources/textures/splode_2",
+                1,
+                1
+            ],
+            "12": [
+                "resources/prefabs/group4.prefab",
+                3
+            ],
+            "13": [
+                "resources/textures/e1",
+                1,
+                1
+            ],
+            "14": [
+                "resources/textures/e8",
+                1,
+                1
+            ],
+            "15": [
+                "resources/textures/e9",
+                1,
+                1
+            ],
+            "16": [
+                "resources/textures/e2",
+                1,
+                1
+            ],
+            "17": [
+                "resources/prefabs/group7.prefab",
+                3
+            ],
+            "18": [
+                "resources/textures/splash_screen",
+                1,
+                1
+            ],
+            "20": [
+                "resources/textures/start_game",
+                1,
+                1
+            ],
+            "21": [
+                "resources/bmpfont/win_score.fnt",
+                2
+            ],
+            "22": [
+                "resources/bmpfont/win_score",
+                1,
+                1
+            ],
+            "23": [
+                "resources/prefabs/group1.prefab",
+                3
+            ],
+            "24": [
+                "resources/prefabs/group8.prefab",
+                3
+            ],
+            "25": [
+                "resources/prefabs/group5.prefab",
+                3
+            ],
+            "26": [
+                "resources/textures/ship_death_7",
+                1,
+                1
+            ],
+            "27": [
+                "resources/textures/starfield",
+                1,
+                1
+            ],
+            "28": [
+                "resources/textures/ship_death_5",
+                1,
+                1
+            ],
+            "29": [
+                "resources/textures/ship_death_3",
+                1,
+                1
+            ],
+            "30": [
+                "resources/textures/ship_death_1",
+                1,
+                1
+            ],
+            "31": [
+                "resources/textures/ship_death_4",
+                1,
+                1
+            ],
+            "32": [
+                "resources/textures/ship_death_6",
+                1,
+                1
+            ],
+            "34": [
+                "resources/textures/ship_death_2",
+                1,
+                1
+            ],
+            "35": [
+                "resources/textures/ship_center",
+                1,
+                1
+            ],
+            "36": [
+                "resources/prefabs/enemy.prefab",
+                3
+            ],
+            "37": [
+                "resources/prefabs/group3.prefab",
+                3
+            ],
+            "38": [
+                "resources/prefabs/group9.prefab",
+                3
+            ],
+            "39": [
+                "resources/prefabs/group6.prefab",
+                3
+            ],
+            "40": [
+                "resources/prefabs/group2.prefab",
+                3
+            ],
+            "41": [
+                "resources/prefabs/bullet.prefab",
+                3
+            ],
+            "4eMb8y8zlKdI3+vOX6fZli": [
+                "resources/bmpfont/win_score.png",
+                0
+            ],
+            "d0Kou6DT9FXbRCAkA8F3Ju": [
+                "resources/sounds/level_1_preloop.mp3",
+                4
+            ],
+            "ddiu+A3r9IL4TmAn+tTiWe": [
+                "resources/sounds/level_2_loop.mp3",
+                4
+            ],
+            "0dB2RTRuNM+qdChqJ+3qYK": [
+                "resources/textures/e1.png",
+                0
+            ],
+            "1f9rQkAhFLDKlZM6uNr8DI": [
+                "resources/textures/e2.png",
+                0
+            ],
+            "17B/mUNGFIpqLrimjszmU9": [
+                "resources/textures/e3.png",
+                0
+            ],
+            "b1dyUHGZxONrdNU0BIOvfs": [
+                "resources/textures/e4.png",
+                0
+            ],
+            "e0MVDa5XxN/ojiI8EqJae8": [
+                "resources/textures/e5.png",
+                0
+            ],
+            "d3FjL+8ehIDLI/tAQqOb2/": [
+                "resources/textures/e6.png",
+                0
+            ],
+            "dfquYT/xdHCLrmzWeqP4io": [
+                "resources/textures/e7.png",
+                0
+            ],
+            "bdpOQM2i5JXaFvSAmgZROO": [
+                "resources/textures/e8.png",
+                0
+            ],
+            "2ePe9Kww1PE6r3pGuyaaKS": [
+                "resources/textures/e9.png",
+                0
+            ],
+            "79VHEd3nZO4JnKC57UHa7v": [
+                "resources/textures/ship_center.png",
+                0
+            ],
+            "87wC8BD5VJVKDjAPXvn9ii": [
+                "resources/textures/ship_death_1.png",
+                0
+            ],
+            "2fPzU4OnxKc6DG3vxc68Q7": [
+                "resources/textures/ship_death_2.png",
+                0
+            ],
+            "ee8CGQnXRKwJbHRHm6Au+n": [
+                "resources/textures/ship_death_3.png",
+                0
+            ],
+            "89Ed6g8XVNMr69rXM4Klr8": [
+                "resources/textures/ship_death_4.png",
+                0
+            ],
+            "95848cCnNIA411LB9s5wtt": [
+                "resources/textures/ship_death_5.png",
+                0
+            ],
+            "52FhPhK/ZFdL9HxuiY5P/i": [
+                "resources/textures/ship_death_6.png",
+                0
+            ],
+            "cet2ln3ZFCXKaRoTWy+SEb": [
+                "resources/textures/ship_death_7.png",
+                0
+            ],
+            "b5ivdFbp1KLIT5XshonTQp": [
+                "resources/textures/splash_screen.jpg",
+                0
+            ],
+            "18HE5TO4hCTJ43FJb3SwC/": [
+                "resources/textures/splode_1.png",
+                0
+            ],
+            "1fvkf2Iy9GZLUCx/tq7G8+": [
+                "resources/textures/splode_2.png",
+                0
+            ],
+            "2a4fbuqZlD67v47SrNShm9": [
+                "resources/textures/splode_3.png",
+                0
+            ],
+            "95Ysv+2S1GE6uzN2/rYKQ1": [
+                "resources/textures/splode_4.png",
+                0
+            ],
+            "ac4ZCwsg5FvJoKgXI+fKPe": [
+                "resources/textures/splode_5.png",
+                0
+            ],
+            "91lnVjKcJPfbtNUTe0H7In": [
+                "resources/textures/splode_6.png",
+                0
+            ],
+            "cfJgeeT0ZCxI2pFmGvbkW/": [
+                "resources/textures/splode_7.png",
+                0
+            ],
+            "27YkqgzkFEq4K/fMNtOgK8": [
+                "resources/textures/starfield.jpg",
+                0
+            ],
+            "60H8ErN9NPUKDWm7JXbSw9": [
+                "resources/textures/start_game.png",
+                0
+            ]
+        },
+        internal: {
+            "02delMVqdBD70a/HSD99FK": [
+                "image/default_sprite_splash.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D",
+        "cc.SpriteFrame",
+        "cc.BitmapFont",
+        "cc.Prefab",
+        "cc.AudioClip"
+    ],
+    launchScene: "db://assets/scenes/home_scene.fire",
+    scenes: [
+        {
+            url: "db://assets/scenes/home_scene.fire",
+            uuid: 19
+        },
+        {
+            url: "db://assets/scenes/game_scene.fire",
+            uuid: 33
+        }
+    ],
+    packedAssets: {
+        "029bad15a": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            12,
+            13,
+            14,
+            15,
+            16
+        ],
+        "02c5502d7": [
+            0,
+            1,
+            2,
+            3,
+            17,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            15,
+            16
+        ],
+        "0434226ce": [
+            18,
+            19,
+            20
+        ],
+        "0482c395c": [
+            21,
+            22
+        ],
+        "0583923de": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            23,
+            15,
+            16
+        ],
+        "06a156c19": [
+            0,
+            1,
+            24,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            15,
+            16
+        ],
+        "090085444": [
+            0,
+            1,
+            2,
+            3,
+            25,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            15,
+            16
+        ],
+        "09c88bfb2": [
+            26,
+            27,
+            28,
+            29,
+            30,
+            31,
+            32,
+            33,
+            34,
+            35
+        ],
+        "0b67ccfbf": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            36,
+            15,
+            16
+        ],
+        "0cd40f608": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            37,
+            13,
+            14,
+            15,
+            16
+        ],
+        "0d3790598": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            38,
+            15,
+            16
+        ],
+        "0db3f7208": [
+            0,
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            39,
+            13,
+            14,
+            15,
+            16
+        ],
+        "0dc30ffa7": [
+            0,
+            1,
+            2,
+            3,
+            40,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10,
+            11,
+            13,
+            14,
+            15,
+            16
+        ],
+        "0dc599ac": [
+            41,
+            "a2MjXRFdtLlYQ5ouAFv/+R"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "03kySPFUVLPbmUxN/lI74l",
+        "0cwS+tYqpNBLGXMzgGVdEk",
+        "23ZzaqsnlBn4iXgOnooX4J",
+        "43CiDjDYJIg4L2dhlnGcsj",
+        "84fdfcnLxD5YR2LhaFK/i8",
+        "85+b5IPQhADKE1EUffv/5T",
+        "86aXofR4lNF6OEjogLykCr",
+        "86pyV7/ENDtJRpTYJyAEs6",
+        "99yM3qvSRAH5Ect6yQ7LIY",
+        "9fZJdxSPtOQaKbnQ10wFDX",
+        "a9B0BtlX9LUauu01K5BRvk",
+        "adN9x/95JAoZpaqBuAnPUt",
+        "b8nNnC3LZOyItzS1OXQgJ7",
+        "beb5LFxUZNNqZz7VoRWlcf",
+        "cbAmCAASpGjZcUzxx9Qp88",
+        "fbekNpafNKJYnfL9D3JTvK",
+        "fb56BmmJ9Lj5ORgA5Sim58",
+        "65wp36wpJFeJALUZzgA2K1",
+        "cbd8+3ULxF8ZdDcUseo0U1",
+        "d2COF3iihB5ok6Ms2smqNh",
+        "dfFsiobvdK+LDt0Y2J3zuB",
+        "89ZOjYbmdEp67XYaAS0br1",
+        "d3Oe3JyepIPbkj7Ivh66/I",
+        "e3p7uR/8tBZL6dknjckiPi",
+        "11c+dpI7hPD7AXZNd0ZIkj",
+        "76XdaIs8lHR6dNz5ZE2/z1",
+        "05qs4XZxRG7KJSEt+ApatY",
+        "20loKeNz1KNovsI5QRbODI",
+        "2fyCRYoDdFGqjhfkodx22n",
+        "44yFbSNoBEhJ8FVicWQlKP",
+        "61MfBUqwdJSKcilval2j8v",
+        "7ao2pYrHRHnplQxHg8wYlb",
+        "7bl1ZYsm9POKWkZk6xbDXq",
+        "90DL+TAPNOYrdeGwgTeQCL",
+        "a8HiGqzltHpKeCOppBVu7n",
+        "c12OiFT5ZJf6w1KREmXU8f",
+        "e5Ont52dBPY4OAM8WkU2KA",
+        "b6ljRhjtRGbbrklhjh+CZg",
+        "eeVX5F98tAMJiDLD97OI4P",
+        "b2fv7sme9DVo6hJNthc8J5",
+        "43ysQTbYJPTbPeFM/OUtky",
+        "04od2xVKVELIQGAwtwMwUY"
+    ],
+    "md5AssetsMap": {
+        "02/029bad15a.json": "45ff3",
+        "02/02c5502d7.json": "b4095",
+        "04/0434226ce.json": "3786f",
+        "04/0482c395c.json": "1594c",
+        "05/0583923de.json": "13d18",
+        "06/06a156c19.json": "1569b",
+        "09/090085444.json": "0c79c",
+        "09/09c88bfb2.json": "e8c4f",
+        "0b/0b67ccfbf.json": "ca33e",
+        "0c/0cd40f608.json": "1151e",
+        "0d/0d3790598.json": "e9c03",
+        "0d/0db3f7208.json": "fbbaf",
+        "0d/0dc30ffa7.json": "b0db9",
+        "0d/0dc599ac.json": "65423",
+        "assets/resources/bmpfont/win_score.png": "b134f",
+        "assets/resources/sounds/level_1_preloop.mp3": "17a39",
+        "assets/resources/sounds/level_2_loop.mp3": "8d7c1",
+        "assets/resources/textures/e1.png": "83cd3",
+        "assets/resources/textures/e2.png": "f75f1",
+        "assets/resources/textures/e3.png": "4bf43",
+        "assets/resources/textures/e4.png": "31637",
+        "assets/resources/textures/e5.png": "53ea9",
+        "assets/resources/textures/e6.png": "9a254",
+        "assets/resources/textures/e7.png": "c0f31",
+        "assets/resources/textures/e8.png": "80764",
+        "assets/resources/textures/e9.png": "fe1fa",
+        "assets/resources/textures/ship_center.png": "9848c",
+        "assets/resources/textures/ship_death_1.png": "7d8d6",
+        "assets/resources/textures/ship_death_2.png": "d53af",
+        "assets/resources/textures/ship_death_3.png": "ebbe1",
+        "assets/resources/textures/ship_death_4.png": "976b6",
+        "assets/resources/textures/ship_death_5.png": "ef4bd",
+        "assets/resources/textures/ship_death_6.png": "33ab5",
+        "assets/resources/textures/ship_death_7.png": "a7baf",
+        "assets/resources/textures/splash_screen.jpg": "b44b0",
+        "assets/resources/textures/splode_1.png": "789cc",
+        "assets/resources/textures/splode_2.png": "9f81a",
+        "assets/resources/textures/splode_3.png": "27c09",
+        "assets/resources/textures/splode_4.png": "b8af5",
+        "assets/resources/textures/splode_5.png": "0060b",
+        "assets/resources/textures/splode_6.png": "de405",
+        "assets/resources/textures/splode_7.png": "c2296",
+        "assets/resources/textures/starfield.jpg": "ae1d7",
+        "assets/resources/textures/start_game.png": "cf695",
+        "internal/image/default_sprite_splash.png": "cea68"
+    }
+};
